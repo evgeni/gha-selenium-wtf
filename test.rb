25773182c5640f7869e38a278bbfb28189bfbe43
Evgeni Golov <evgeni@golov.de>
2024-02-02 09:04:57 +0100

    headless

--- test.rb
+++ test.rb
@@ -1,6 +1,12 @@
 require 'selenium-webdriver'
 
-driver = Selenium::WebDriver.for :chrome
+options = Selenium::WebDriver::Chrome::Options.new
+options.args << '--disable-gpu'
+options.args << '--no-sandbox'
+options.args << '--window-size=1024,768'
+options.args << '--headless'
+
+driver = Selenium::WebDriver.for(:chrome, options: options)
 
 driver.get('https://www.selenium.dev/selenium/web/web-form.html')
 
